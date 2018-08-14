00a53368e7be5b82539457203f424eb7139b73a7
winex01 <winnie131212592@gmail.com>
2018-08-14 19:55:10 +0800

    fix basic register and refactor master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,9 +4,11 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  
-  <meta name="_token" content="ihxnr0qfxkKuafcqpf3osuHIvio17qfokFQnXrgo" />
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
   <title>{{ env('APP_NAME') }}</title>
 
